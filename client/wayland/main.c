294fe70557415c21f16a29ef2333c44a2ea7c11b
Philipp Brüschweiler <blei42@gmail.com>
2012-07-17 15:57:38 +0200

    wayland: empty template

---
+++ client/wayland/main.c
@@ -0,0 +1,30 @@
+/* -*- mode: C; c-basic-offset: 4; indent-tabs-mode: nil; -*- */
+/* vim:set et sts=4: */
+/* ibus
+ * Copyright (C) 2012 Philipp Brüschweiler <blei42@gmail.com>
+ *
+ * This tool is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <stdlib.h>
+
+#include <ibus.h>
+
+int
+main (int argc, char **argv)
+{
+        exit (EXIT_SUCCESS);
+}
