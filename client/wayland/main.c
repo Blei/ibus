63fad2a7500ceb288b1a2d2d6a0f5c57a779b446
Philipp Brüschweiler <blei42@gmail.com>
2012-07-17 21:32:13 +0200

    wayland: integrate enough functionality for a demo. but.
    the weston and the glib/gtk main loops have to be integrated.
    as the code works now, only the weston loop runs and as a
    consequence no ibus signals are delivered.

--- client/wayland/main.c
+++ client/wayland/main.c
@@ -19,9 +19,13 @@
  * Boston, MA 02111-1307, USA.
  */
 
+#include <assert.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+
+#include <gdk/gdk.h>
 
 #include <ibus.h>
 
@@ -43,19 +47,32 @@ struct ibus_ime {
     } xkb;
 
     IBusBus *bus;
+    IBusInputContext *context;
+
+    char *preedit_string;
+    IBusAttrList *preedit_attrs;
+    uint32_t preedit_cursor;
+    bool preedit_visible;
+    bool preedit_started;
+
+    bool focused;
 };
 
 static void
 reset_ibus_ime(struct ibus_ime *ime)
 {
-    // TODO
-}
-
-static void
-ime_handle_key(struct ibus_ime *ime, uint32_t key, uint32_t sym,
-	       enum wl_keyboard_key_state state)
-{
-    // TODO
+    if (ime->preedit_string) {
+        free(ime->preedit_string);
+        ime->preedit_string = NULL;
+    }
+    if (ime->preedit_attrs) {
+        g_object_unref (ime->preedit_attrs);
+        ime->preedit_attrs = NULL;
+    }
+    ime->preedit_cursor = 0;
+    ime->preedit_visible = false;
+    ime->preedit_started = false;
+    ime->focused = false;
 }
 
 static void
@@ -63,6 +80,12 @@ input_method_reset(void *data,
                    struct input_method *input_method)
 {
     struct ibus_ime *ime = data;
+
+    // HACK: this is here because wayland/weston doesn't do ime focus yet
+    if (ime->focused)
+        ibus_input_context_focus_out(ime->context);
+
+    ibus_input_context_reset(ime->context);
     reset_ibus_ime(ime);
 }
 
@@ -124,6 +147,41 @@ input_method_keymap(void *data,
 }
 
 static void
+translate_key(GdkEventKey *event,
+              xkb_keysym_t sym,
+              struct ibus_ime *ime,
+              enum wl_keyboard_key_state state,
+              uint32_t keycode,
+              uint32_t time)
+{
+    event->time = time;
+    event->window = NULL;
+    event->send_event = 0; // FIXME what to do?
+    event->type = state == WL_KEYBOARD_KEY_STATE_RELEASED ? GDK_KEY_RELEASE : GDK_KEY_PRESS;
+    event->group = 0; // FIXME this should be possible to find out
+    event->hardware_keycode = keycode;
+
+    event->state = 0;
+    if (ime->modifiers & MOD_CONTROL_MASK)
+        event->state |= GDK_CONTROL_MASK;
+    if (ime->modifiers & MOD_SHIFT_MASK)
+        event->state |= GDK_SHIFT_MASK;
+    if (ime->modifiers & MOD_ALT_MASK)
+        event->state |= GDK_MOD1_MASK;
+    if (state == WL_KEYBOARD_KEY_STATE_RELEASED)
+        event->state |= IBUS_RELEASE_MASK;
+
+    // Looks wrong, but is equivalent to what the XIM backend does.
+    event->is_modifier = 0;
+    event->string = NULL;
+    event->length = 0;
+
+    // HACK: The gdk keysyms are auto generated from the xkb ones, this should
+    // be equivalent. Did some spot checks, everything looks correct.
+    event->keyval = sym;
+}
+
+static void
 input_method_key(void *data,
                  struct input_method *input_method,
                  uint32_t serial,
@@ -132,7 +190,7 @@ input_method_key(void *data,
                  uint32_t state_w)
 {
     struct ibus_ime *ime = data;
-    //fprintf(stderr, "received key %d\n", key);
+    fprintf(stderr, "received key %d\n", key);
 
     // XXX: copied from window.c
     uint32_t code, num_syms;
@@ -140,6 +198,12 @@ input_method_key(void *data,
     const xkb_keysym_t *syms;
     xkb_keysym_t sym;
     xkb_mod_mask_t mask;
+
+    // HACK: this is here because wayland/weston doesn't do ime focus yet
+    if (!ime->focused) {
+        //ibus_input_context_focus_in(ime->context);
+        ime->focused = true;
+    }
 
     code = key + 8;
     if (!ime->xkb.state)
@@ -161,8 +225,23 @@ input_method_key(void *data,
     sym = XKB_KEY_NoSymbol;
     if (num_syms == 1)
         sym = syms[0];
+    // end of copied code
 
-    ime_handle_key(ime, key, sym, state);
+    GdkEventKey event;
+    translate_key(&event, sym, ime, state, key, time);
+
+    gboolean retval = ibus_input_context_process_key_event (
+        ime->context,
+        event.keyval,
+        event.hardware_keycode - 8,
+        event.state);
+
+    fprintf(stderr, "input has %sbeen processed\n", retval ? "" : "not ");
+
+    if (!retval) {
+        // The key event could not be processed correctly.
+        // TODO forward the key event to the application
+    }
 }
 
 static void
@@ -205,6 +284,130 @@ global_handler(struct wl_display *display, uint32_t id,
 }
 
 static void
+_context_commit_text_cb (IBusInputContext *context,
+                         IBusText         *text,
+                         struct ibus_ime  *ime)
+{
+    g_assert (IBUS_IS_INPUT_CONTEXT (context));
+    g_assert (IBUS_IS_TEXT (text));
+    g_assert (ime != NULL);
+
+    fprintf(stderr, "commit with text '%s'\n", text->text);
+
+    input_method_commit_string(ime->input_method, text->text, -1);
+}
+
+static void
+_context_forward_key_event_cb (IBusInputContext *context,
+                               guint             keyval,
+                               guint             keycode,
+                               guint             state,
+                               struct ibus_ime  *ime)
+{
+    g_assert (ime);
+
+    fprintf(stderr, "forward key with keyval '%d'\n", keyval);
+
+    // TODO implement this as soon as wayland/weston supports it
+}
+
+static void
+_update_preedit(struct ibus_ime *ime)
+{
+    if (ime->preedit_visible) {
+        if (!ime->preedit_started) {
+            ime->preedit_started = true;
+        }
+        input_method_preedit_string(ime->input_method, ime->preedit_string,
+                                    ime->preedit_cursor);
+    } else if (ime->preedit_started) {
+        input_method_preedit_string(ime->input_method, "", 0);
+        ime->preedit_started = false;
+    }
+}
+
+static void
+_context_update_preedit_text_cb (IBusInputContext *context,
+                                 IBusText         *text,
+                                 gint              cursor_pos,
+                                 gboolean          visible,
+                                 struct ibus_ime  *ime)
+{
+    g_assert (IBUS_IS_INPUT_CONTEXT (context));
+    g_assert (IBUS_IS_TEXT (text));
+    g_assert (ime);
+
+    fprintf(stderr, "update preedit with string '%s'\n", text->text);
+
+    if (ime->preedit_string) {
+        free(ime->preedit_string);
+    }
+    ime->preedit_string = strdup(text->text);
+
+    if (ime->preedit_attrs) {
+        g_object_unref (ime->preedit_attrs);
+    }
+
+    g_object_ref(text->attrs);
+    ime->preedit_attrs = text->attrs;
+
+    ime->preedit_cursor = cursor_pos;
+    ime->preedit_visible = visible;
+
+    _update_preedit (ime);
+}
+
+static void
+_context_show_preedit_text_cb (IBusInputContext *context,
+                               struct ibus_ime  *ime)
+{
+    g_assert (IBUS_IS_INPUT_CONTEXT (context));
+    g_assert (ime);
+
+    fprintf(stderr, "show preedit\n");
+
+    ime->preedit_visible = true;
+    _update_preedit (ime);
+}
+
+static void
+_context_hide_preedit_text_cb (IBusInputContext *context,
+                               struct ibus_ime  *ime)
+{
+    g_assert (IBUS_IS_INPUT_CONTEXT (context));
+    g_assert (ime);
+
+    fprintf(stderr, "hide preedit\n");
+
+    ime->preedit_visible = false;
+    _update_preedit (ime);
+}
+
+static void
+_context_enabled_cb (IBusInputContext *context,
+                     struct ibus_ime  *ime)
+{
+    g_assert (IBUS_IS_INPUT_CONTEXT (context));
+    g_assert (ime);
+
+    fprintf(stderr, "enabled\n");
+
+    // Is there anything that needs to be done here?
+}
+
+static void
+_context_disabled_cb (IBusInputContext *context,
+                      struct ibus_ime  *ime)
+{
+    g_assert (IBUS_IS_INPUT_CONTEXT (context));
+    g_assert (ime);
+
+    fprintf(stderr, "disabled\n");
+
+    reset_ibus_ime(ime);
+}
+
+static void
 _bus_disconnected_cb (IBusBus  *bus,
                       gpointer  user_data)
 {
@@ -214,17 +417,51 @@ _bus_disconnected_cb (IBusBus  *bus,
 }
 
 static void
-_init_ibus (IBusBus **bus)
+_init_ibus_bus (struct ibus_ime *ime)
 {
-    if (*bus != NULL)
+    if (ime->bus != NULL)
         return;
 
     ibus_init ();
 
-    *bus = ibus_bus_new ();
+    ime->bus = ibus_bus_new ();
 
-    g_signal_connect (*bus, "disconnected",
+    g_signal_connect (ime->bus, "disconnected",
                       G_CALLBACK (_bus_disconnected_cb), NULL);
+}
+
+static void
+_init_ibus_context (struct ibus_ime *ime)
+{
+    ime->context = ibus_bus_create_input_context (ime->bus, "wayland");
+    assert(ime->context && "ibus context couldn't be created");
+
+    g_signal_connect (ime->context, "commit-text",
+                        G_CALLBACK (_context_commit_text_cb), ime);
+    g_signal_connect (ime->context, "forward-key-event",
+                        G_CALLBACK (_context_forward_key_event_cb), ime);
+
+    g_signal_connect (ime->context, "update-preedit-text",
+                        G_CALLBACK (_context_update_preedit_text_cb), ime);
+    g_signal_connect (ime->context, "show-preedit-text",
+                        G_CALLBACK (_context_show_preedit_text_cb), ime);
+    g_signal_connect (ime->context, "hide-preedit-text",
+                        G_CALLBACK (_context_hide_preedit_text_cb), ime);
+    g_signal_connect (ime->context, "enabled",
+                        G_CALLBACK (_context_enabled_cb), ime);
+    g_signal_connect (ime->context, "disabled",
+                        G_CALLBACK (_context_disabled_cb), ime);
+
+    // TODO add IBUS_CAP_SURROUNDING_TEXT once wayland/weston supports that
+    ibus_input_context_set_capabilities (ime->context,
+                                         IBUS_CAP_FOCUS | IBUS_CAP_PREEDIT_TEXT);
+}
+
+static void
+init_ibus (struct ibus_ime *ime)
+{
+    _init_ibus_bus(ime);
+    _init_ibus_context(ime);
 }
 
 int
@@ -235,7 +472,7 @@ main(int argc, char *argv[])
 
     g_type_init();
 
-    _init_ibus (&ime.bus);
+    init_ibus(&ime);
 
     if (!ibus_bus_is_connected (ime.bus)) {
         g_warning ("Can not connect to ibus daemon");
@@ -261,6 +498,8 @@ main(int argc, char *argv[])
 
     display_set_user_data(ime.display, &ime);
 
+    // XXX integrate the weston & glib main loops!
+    // maybe add the epolls called in display_run as a GSoure?
     display_run(ime.display);
 
     g_object_unref (ime.bus);
